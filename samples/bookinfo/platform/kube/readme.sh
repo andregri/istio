f52520aa9ce5875f91786738faf2099343280977
Andrea Grillo <andrea.grillo96@live.com>
2024-09-06 17:49:55 +0200

    create mysql db using statefulset

---
+++ samples/bookinfo/platform/kube/readme.sh
@@ -0,0 +1,13 @@
+kubectl create ns bookinfo
+kubectl apply -f bookinfo.yaml -n bookinfo
+kubectl apply -f bookinfo-mysql-statefulset.yaml -n bookinfo
+
+kubectl get po -n bookinfo -w
+
+kubectl apply -f bookinfo-ratings-v2-mysql.yaml -n bookinfo
+
+kubectl port-forward svc/productpage -n bookinfo 9080:9080
+
+kubectl logs -n bookinfo -c mysql mysql-0
+
+kubectl delete ns bookinfo
